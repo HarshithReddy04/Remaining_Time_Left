f58b01f7435e802e0b4a078f39181305632f94ec
Harshith Reddy <118986591+HarshithReddy04@users.noreply.github.com>
2023-08-12 10:35:35 +0530

    Add files via upload

---
+++ Remaining_time.py
@@ -0,0 +1,11 @@
+#your time left assuming you live upto 90 years
+age = input("What is your current age? ")
+total_years = 90
+left_years = total_years - int(age)
+x = left_years * 365
+y = left_years *52
+z = left_years * 12
+print(f"You have: \n{x} days, \n{y} weeks, \n{z} months left.")
+
+
+
